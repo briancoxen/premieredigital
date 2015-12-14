cfe0128bb4bec6f859920b74d931aa377f22acd5
root <root@ip-172-31-19-94.us-west-2.compute.internal>
2015-12-14 02:23:58 +0000

    Add error message for MD5 mismatch

--- static/js/app.js
+++ static/js/app.js
@@ -16,11 +16,26 @@ premiereApp.directive('fileModel', ['$parse', function ($parse) {
     };
 }]);
 
-premiereApp.service('fileUpload', ['$http', function ($http) {
-    this.uploadFileToUrl = function(cb, file, name, uploadUrl){
+premiereApp.service('premiereService', ['$http', function ($http) {
+    this.uploadXMLFile = function(cb, file, uploadUrl){
         var fd = new FormData();
         fd.append('file', file);
-        fd.append('name', name);
+        $http.post(uploadUrl, fd, {
+            transformRequest: angular.identity,
+            headers: {'Content-Type': undefined}
+        })
+        .success(function(data){
+            cb(data);
+        })
+        .error(function(){
+        });
+    }
+
+    this.uploadMediaFile = function(cb, file, id, checksum, uploadUrl){
+        var fd = new FormData();
+        fd.append('file', file);
+        fd.append('id', id);
+        fd.append('checksum', checksum);
         $http.post(uploadUrl, fd, {
             transformRequest: angular.identity,
             headers: {'Content-Type': undefined}
@@ -42,43 +57,87 @@ premiereApp.service('fileUpload', ['$http', function ($http) {
     }
 }]);
 
-premiereApp.controller('premiereCtrl', ['$scope', 'fileUpload', function($scope, fileUpload, movies){
+premiereApp.controller('premiereCtrl', ['$scope', 'premiereService', function($scope, premiereService, movies){
+    $scope.detailsPage = false;
     $scope.xmlUploader = true;
     $scope.AllMovies = true;
     $scope.Pagination = true;
     $scope.Movies = [];
 
-    fileUpload.getAll(function(cb) {
+    premiereService.getAll(function(cb) {
         angular.forEach(cb, function(values) {
             $scope.Movies.push(values);
         });
     });
 
-    $scope.uploadFile = function(){
+    $scope.getAllData = function() {
+        $scope.error = false;
+        $scope.MovieDetails = false;
+        $scope.detailsPage = false;
+        $scope.AllMovies = true;
+        $scope.xmlUploader = true;
+        $scope.Pagination = true;
+        $scope.Movies = [];
+        premiereService.getAll(function(cb) {
+            angular.forEach(cb, function(values) {
+                $scope.Movies.push(values);
+            });
+        });
+    }
+
+    $scope.uploadXMLFile = function(){
         $scope.xmlUploader = false;
+        $scope.AllMovies = false;
+
         $scope.xmlUploadSpinner = true;
+
         var file = $scope.file;
         var uploadUrl = "http://52.25.116.171:8080/uploadXML";
 
-        fileUpload.uploadFileToUrl(function(cb) {
+        premiereService.uploadXMLFile(function(cb) {
             $scope.xmlUploader = true;
             $scope.xmlUploadSpinner = false;
+            $scope.AllMovies = true;
             $scope.Movies = [];
             if (cb.success === "0"){
-                fileUpload.getAll(function(cb) {
+                premiereService.getAll(function(cb) {
                     angular.forEach(cb, function(values) {
                         $scope.Movies.push(values); 
                     });
                 }); 
             }
-        }, file, $scope.name, uploadUrl);
+        }, file, uploadUrl);
     };
 
-    $scope.showDetails = function(id, Title, MD5, Director, ReleaseDate, Length, Type, Description) {
+    $scope.uploadMediaFile = function(id, checksum){
+        $scope.mediaUploadSpinner = true;
+        $scope.mediaUploadForm = false;
+        var file = $scope.file;
+        var uploadUrl = "http://52.25.116.171:8080/uploadMedia";
+
+        premiereService.uploadMediaFile(function(cb) {
+            $scope.mediaUploadSpinner = false;
+
+            if (cb.status === "0"){
+                $scope.error = false;
+                $scope.details.File = cb.File;
+            } else {
+                $scope.mediaUploadForm = true;
+                $scope.error = true;
+                $scope.errorMsg = "The uploaded file's checksum did not match, please try again! " + cb.error;
+            }
+        }, file, id, checksum, uploadUrl);
+    };
+
+    $scope.showDetails = function(id, Title, MD5, Director, ReleaseDate, Length, Type, Description, File) {
+        $scope.mediaUploadForm = true;
         $scope.AllMovies = false;
         $scope.Pagination = false;
-        $scope.MovieDetails = true;
+        $scope.xmlUploader = false;
 
+        $scope.MovieDetails = true;
+        $scope.detailsPage = true;
+  
         $scope.details = {
             id : id,
             Title : Title,
@@ -88,6 +147,7 @@ premiereApp.controller('premiereCtrl', ['$scope', 'fileUpload', function($scope,
             Length : Length,
             Type : Type,
             Description : Description,
+            File : File,
         };
     };
 }]);
